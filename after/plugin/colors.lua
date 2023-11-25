73554b3b56f2743c1220acb3761427cd280dfa5a
Genesio S Pacheco <genesio.s.p.28@gmail.com>
2023-11-24 23:48:35 -0300

    vscode theme

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,4 +1,19 @@
-require("catppuccin").setup({
-  flavour = "mocha"
+vim.o.background = 'dark'
+
+local c = require('vscode.colors').get_colors()
+
+require("vscode").setup({
+  transparent = true,
+  italic_coments = true,
+  disable_nvimtree_bg = true,
+
+  color_overrides = {
+    vscLineNumber = '#666666'
+  },
+
+  group_overrides = {
+    Cursor = { fg = c.vscDarkBlue, bg = c.vscLightGreen, bold = true }
+  }
 })
-vim.cmd.colorscheme("catppuccin")
+
+require('vscode').load()
