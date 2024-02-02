1dd432dd0f9be28cc2bdc2dba2dd8c71c8ebd2ab
Genésio <genesio.s.p.28@gmail.com>
2024-02-02 12:17:00 -0300

    lazy-lock and a space

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -4,7 +4,7 @@
 vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'go', 'lua', 'tsx', 'javascript','prisma', 'typescript', 'vimdoc', 'vim', 'bash', 'json', 'markdown', 'sql', 'solidity'},
+    ensure_installed = { 'go', 'lua', 'tsx', 'javascript', 'prisma', 'typescript', 'vimdoc', 'vim', 'bash', 'json', 'markdown', 'sql', 'solidity'},
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = true,
